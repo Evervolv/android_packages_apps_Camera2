4305b81751a4db8359a31975da77476475c6dbb4
nadlabak <pavel@doshaska.net>
2014-12-29 01:00:50 +0100

    Camera2: Stop using GPS when going to background
    Change-Id: Iacc46d3934c4a9fea7f01666334aaec213bb902b

--- src/com/android/camera/CameraActivity.java
+++ src/com/android/camera/CameraActivity.java
@@ -1857,6 +1857,7 @@ public class CameraActivity extends QuickActivity
         mCurrentModule.pause();
         mOrientationManager.pause();
         mPanoramaViewHelper.onPause();
+        mLocationManager.recordLocation(false);
 
         mLocalImagesObserver.setForegroundChangeListener(null);
         mLocalImagesObserver.setActivityPaused(true);
